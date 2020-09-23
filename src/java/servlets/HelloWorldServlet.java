80c13d448860e9f2677518b98f832c7606fcb2fd
820203 <820203@SAIT224250IT.ACDM.DS.SAIT.CA>
2020-09-23 12:47:45 -0600

    Added validation, added validation to the form to make sure the user inputs both last & first names.

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -37,6 +37,13 @@ public class HelloWorldServlet extends HttpServlet
         request.setAttribute("firstname", firstname);
         request.setAttribute("lastname", lastname);
         
+        if(firstname == null || firstname.equals("") || lastname == null || lastname.equals(""))
+        {
+            getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp")
+                 .forward(request, response);
+            return;
+        }
+        
         getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
                 .forward(request, response);
     }
